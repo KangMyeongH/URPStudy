748c56336115533a7940905734e6dafb10378d9b
KangMyeongH <71129440+KangMyeongH@users.noreply.github.com>
2023-04-23 22:16:28 +0900

    Unity SetUP
    1. HLSL 파일열때 VSCODE로 열리게 설정 해둠
    2. 비싼거 삼

---
+++ ShaderStudy/Assets/Scenes/DefaultNewHLSLName.hlsl
@@ -0,0 +1,28 @@
+﻿#ifndef DEFAULTNEWHLSLNAME_INCLUDED
+#define DEFAULTNEWHLSLNAME_INCLUDED
+
+void DefaultNewHLSLName_float(out float3 Out)
+{
+    Out = float3(1, 1, 1);
+
+    // [Option]
+    // #ifdef SHADERGRAPH_PREVIEW
+    
+    // #else
+        
+    // #endif
+}
+
+void DefaultNewHLSLName_half(out half3 Out)
+{
+    Out = half3(1, 1, 1);
+
+    // [Option]
+    // #ifdef SHADERGRAPH_PREVIEW
+    
+    // #else
+        
+    // #endif
+}
+
+#endif
---
+++ ShaderStudy/Assets/Scenes/DefaultNewSOScriptName.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "", menuName = "", order = int.MaxValue)]
+public class DefaultNewSOScriptName : ScriptableObject
+{
+
+}
